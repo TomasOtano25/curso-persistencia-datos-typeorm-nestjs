bdad5987aa0d868752f12a571a474b2b605b0ca5
Tomas Garcia <tomasotano25@gmail.com>
2022-10-16 15:03:29 -0400

    clase 13 cambiar a mysql

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -15,9 +15,9 @@ const API_KEY_PROD = 'PROD_123456';
     TypeOrmModule.forRootAsync({
       inject: [config.KEY],
       useFactory: (configService: ConfigType<typeof config>) => {
-        const { user, host, dbName, password, port } = configService.postgres;
+        const { user, host, dbName, password, port } = configService.mysql;
         return {
-          type: 'postgres',
+          type: 'mysql',
           host,
           port,
           username: user,
